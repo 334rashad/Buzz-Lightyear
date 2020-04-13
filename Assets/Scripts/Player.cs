a84f561dd35d15b7043dff5a0f8ae575703295b6
Rashadat Mirzayev <334rashad@gmail.com>
2020-04-13 23:55:46 +0200

    player move up down left right borders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float playerMoveSpeed = 15f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).y;
     }
 
     // Update is called once per frame
@@ -22,8 +36,10 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * playerMoveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed;
-        var newXPosition = transform.position.x + deltaX;
+
+        var newXPosition = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         var newYPosition = transform.position.y + deltaY;
+
         transform.position = new Vector2(newXPosition, newYPosition);
     }
 }
