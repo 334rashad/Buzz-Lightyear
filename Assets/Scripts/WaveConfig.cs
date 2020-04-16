46ed2a215123b553bb9d04b48e62571824c32312
Rashadat Mirzayev <334rashad@gmail.com>
2020-04-16 23:59:44 +0200

    waves

---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Configuration")]
+
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = .3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float movingSpeed = 2f;
+
+    public GameObject EnemyPrefab { get => enemyPrefab; set => enemyPrefab = value; }
+    public List<Transform> GetWaypoints()
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints; 
+    }
+public float TimeBetweenSpawns { get => timeBetweenSpawns; set => timeBetweenSpawns = value; }
+    public float SpawnRandomFactor { get => spawnRandomFactor; set => spawnRandomFactor = value; }
+    public int NumberOfEnemies { get => numberOfEnemies; set => numberOfEnemies = value; }
+    public float MovingSpeed { get => movingSpeed; set => movingSpeed = value; }
+}
